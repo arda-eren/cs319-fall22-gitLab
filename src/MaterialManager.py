6ece21e29e4c2720387e97f79e862e957f96b0a9
arda-eren <a.eren@ug.bilkent.edu.tr>
2021-10-20 19:00:11 +0300

    Revert "add vibranium material"
    This reverts commit d1f0d1b20197ef3b42de19391bda73a8f15b46b7.

--- src/MaterialManager.py
+++ src/MaterialManager.py
@@ -19,12 +19,8 @@ class MaterialManager:
                 self.carManager.setMaterial(materialType.Carbon)
                 returnText = "Material is changed to Carbon."
 
-            case materialType.Vibranium:
-                self.carManager.setMaterial(materialType.Vibranium)
-                returnText = "Material is changed to Vibranium."
-
             case _:
-                returnText = "Material selection is invalid."
+                returnText = "Material seletion is invalid."
 
         return self._announceMaterial(returnText)
 
